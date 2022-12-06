6fe185036652344b2f2d648d26e5af9d6a3d2ab1
Camilo María <camilo_jose08@hotmail.com>
2022-12-05 23:54:03 -0400

    test

--- test.py
+++ test.py
@@ -9,16 +9,18 @@ class TestCRUD(unittest.TestCase):
         pass
     
     def test_insertar(self):
-        actual = paises.modificar_pais("RPD", "Republica Pacheca Dominicana", "Pachequita", "RPS")
+        actual = paises.insertar_pais("RPD", "Republica Pacheca Dominicana", "Pachequita", "RPS")
         self.assertEqual(actual, 1) 
   
+    def test_delete(self):
+        actual = paises.eliminar_pais(7)
+        self.assertEqual(actual, 1)
+        
     def test_modificar(self):
         actual = paises.modificar_pais(7, "PLD", "Pelicanos La Dominicana", "Pelicanos", "PLS")
         self.assertEqual(actual, 1) 
 
-    def test_delete(self):
-        actual = paises.eliminar_pais(8)
-        self.assertEqual(actual, 1)
+
 
 
 if __name__ == '__main__':
