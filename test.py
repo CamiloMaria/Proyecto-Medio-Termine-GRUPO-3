82bd62fff60c365165cac573ee7897389818407f
Camilo María <camilo_jose08@hotmail.com>
2022-12-05 23:45:14 -0400

    Agregue pruebas unitarias

--- test.py
+++ test.py
@@ -5,9 +5,21 @@ from Paises import Paises
 paises = Paises()
 
 class TestCRUD(unittest.TestCase):
-    def test_delete():
-        # Delete an existing item and verify that it has been removed from the database
-        assert paises.eliminar_pais(10)
-
+    def setUp(self):
+        pass
     
+    def test_insertar(self):
+        actual = paises.modificar_pais("RPD", "Republica Pacheca Dominicana", "Pachequita", "RPS")
+        self.assertEqual(actual, 1) 
+  
+    def test_modificar(self):
+        actual = paises.modificar_pais(7, "PLD", "Pelicanos La Dominicana", "Pelicanos", "PLS")
+        self.assertEqual(actual, 1) 
 
+    def test_delete(self):
+        actual = paises.eliminar_pais(8)
+        self.assertEqual(actual, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
