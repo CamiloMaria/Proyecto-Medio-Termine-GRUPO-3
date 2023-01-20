06373ed1b9c654f16e45dc710d1127c2678d9035
Camilo María <camilo_jose08@hotmail.com>
2023-01-20 00:00:27 -0400

    Agregue el protocolo UDP

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from Paises import Paises
 import os
+import socket
+from Paises import Paises
 
 paises = Paises()
+
+#Declarar la direccion y puerto de UDP
+UDP_IP = "localhost"
+UDP_PORT = 3005
 
 menu='''\n                                  
 1) Insertar País      
@@ -9,6 +14,16 @@ menu='''\n
 3) Modificar País      
 4) Imprimir Paises    
 5) Salir'''
+
+def send_data(pais):
+    # Crear un socket para enviar datos
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Convertir el objeto país a una cadena de texto
+    pais_data = str(pais)
+
+    # Enviar los datos del país al servidor UDP
+    sock.sendto(pais_data.encode(), (UDP_IP, UDP_PORT))
 
 def main():
 
@@ -25,18 +40,21 @@ def main():
         if opcion == '1':
             print("\n*Eligio Insertar Paises*\n")
 
+            
             ISO3 = input("Introduce la clave ISO3 del nuevo País: ")
             CountryName = input("Introduce el nombre del nuevo País: ")
             Capital = input("Introduce la capital del nuevo País: ")
             CurrencyCode = input("Introduce el código de su moneda: ")
 
             r = paises.insertar_pais(ISO3,CountryName,Capital,CurrencyCode)
-            
+
             if(r==0):
                 print("-> No se pudo insertar el páis...")
             else:
                 print("-> El páis se insertó correctamente")
-            
+                #Envia los datos al servidor UDP
+                send_data(r) 
+             
         elif opcion == '2':
             print("\n*Eliminar Paises*\n")
 
@@ -47,6 +65,9 @@ def main():
                 print("-> El páis no existe")
             else:
                 print("-> El páis se eliminó correctamente")
+                #Envia los datos al servidor UDP
+                send_data(r)
+
 
         elif opcion == '3':
             print("\n*Modificar Paises*\n")
@@ -71,6 +92,8 @@ def main():
                     print("-> Error al modificar el país...")
                 else:
                     print("-> El páis se modificó correctamente")
+                    #Envia los datos al servidor UDP
+                    send_data(pais)
 
         elif opcion == '4':
             print("\n*Imprimir Paises*\n")
@@ -82,7 +105,6 @@ def main():
 
         else:            
             print("Opcion no valida")
-
 
 if __name__ == "__main__":
     main()
