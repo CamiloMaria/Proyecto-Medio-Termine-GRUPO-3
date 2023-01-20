7a8af9021ec67259ea2068d6c484b81984625039
Camilo María <camilo_jose08@hotmail.com>
2023-01-20 17:10:49 -0400

    .

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from Paises import Paises
+
+paises = Paises()
+
+class TestCRUD(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_insertar(self):
+        actual = paises.insertar_pais("RPD", "Republica Pacheca Dominicana", "Pachequita", "RPS")
+        self.assertEqual(actual, 1) 
+  
+    def test_delete(self):
+        actual = paises.eliminar_pais(7)
+        self.assertEqual(actual, 1)
+        
+    def test_modificar(self):
+        actual = paises.modificar_pais(7, "PLD", "Pelicanos La Dominicana", "Pelicanos", "PLS")
+        self.assertEqual(actual, 1) 
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
